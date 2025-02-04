22196d7848a622bae8bf981fca5a64c6e17f3440
MAZZA <mazza8azzouz@gmail.com>
2025-02-04 22:45:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
 const path="./data.json";
-const date=moment().format();
+const markCommit=(x,y)=>{
+const date=moment()
+  .subtract(1,"y")
+  .add(1,"d")
+  .add(x,"w")
+  .add(y,"d")
+  .format();
 const data={
   date:date,
 };
 jsonfile.writefile(path,data,()=>{
   simpleGit().add([path]).commit(date,{'--date':date}).push();
 });
-
+};
